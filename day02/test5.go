6cee1a7cd263c60d2b9f5fe8db08a8847591e46e
gongchen78 <546591178@qq.com>
2021-11-29 10:45:42 +0800

    增加switch草稿

--- day02/test5.go
+++ day02/test5.go
@@ -7,13 +7,25 @@ import "fmt"
 */
 
 func main() {
-	money := 20
-	switch money {
-	case 20:
-		fmt.Println("20")
-	case 30:
-		fmt.Println("30")
-	default:
-		fmt.Println("没钱")
+	//money := 20
+	//switch money {
+	//case 20:
+	//	fmt.Println("20")
+	//	fallthrough
+	//case 30:
+	//	fmt.Println("30")
+	//	fallthrough
+	//default:
+	//	fmt.Println("没钱")
+	//}
+	var money interface{} = 100
+	switch newMoney := money.(type) {
+	case int:
+		tmpMoney := newMoney + 3.0 //3.1234报错
+		fmt.Println("int", tmpMoney)
+	case float64:
+		fmt.Println("float64")
+	case string:
+		fmt.Println("string")
 	}
 }
