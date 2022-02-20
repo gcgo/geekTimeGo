3f1468cb303f092aed1d5ba28d85a4e51ee2977a
gcgo <546591178@qq.com>
2022-02-20 23:53:22 +0800

    作业4提交

--- homework4/writebych.go
+++ homework4/writebych.go
@@ -16,6 +16,7 @@ func main() {
 			P := <-chP
 			Ps = append(Ps, P)
 			sort.Sort(Ps)
+			log.Println("打印最新排名：")
 			for _, p := range Ps {
 				log.Println(p.name)
 			}
@@ -27,15 +28,22 @@ func main() {
 			p := Person{
 				name:   "person+" + strconv.Itoa(i),
 				weight: float64(rand.Intn(65) + 45),
-				tall:   rand.Float64()*0.5 + 1.5,
+				tall:   rand.Float64()*0.4 + 1.6,
 				sex:    float64(rand.Intn(2)),
 				age:    rand.Intn(40) + 18,
 			}
 			p.calcBmi()
 			p.calcFatRate()
-			log.Printf("姓名%s: ,性别：%f ,年龄：%d ,身高：%f ,体重:%f ,BMI：%f ,体脂率: %f \n", p.name, p.sex, p.age, p.tall, p.weight, p.bmi, p.fatRate)
+			var sex string
+			if p.sex == 1 {
+				sex = "男"
+			} else {
+				sex = "女"
+			}
+			log.Printf("新成员加入！！姓名%s: ,性别：%s ,年龄：%d ,身高：%f米 ,体重:%f公斤 ,BMI：%f ,体脂率: %f \n", p.name, sex, p.age, p.tall, p.weight, p.bmi, p.fatRate)
 			chP <- p
 		}(i)
+		time.Sleep(500 * time.Microsecond)
 	}
-	time.Sleep(5 * time.Second)
+	time.Sleep(1 * time.Second)
 }
